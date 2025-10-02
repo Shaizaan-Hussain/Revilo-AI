64b1946ddfbefcc92fe23b2bdb5c41df999aec90
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-10-02 15:45:16 +0530

    Completed Hero Section top part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navabr from '../components/Navabr'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
     <Navabr/>
+    <Hero/>
     </>
   )
 }
