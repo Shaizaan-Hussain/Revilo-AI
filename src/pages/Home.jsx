619345fdab1e3a257648f6ee8de6a111f0f97145
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-10-03 16:05:11 +0530

    Completed Tool list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navabr from '../components/Navabr'
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
   return (
     <>
     <Navabr/>
     <Hero/>
+    <AiTools/>
     </>
   )
 }
